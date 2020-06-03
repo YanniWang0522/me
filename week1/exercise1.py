33129713783cd938ebc1e6c5c019bcca15b8de2c
Yanni Wang <yanniwang@Yannis-MacBook-Pro.local>
2020-06-03 12:37:07 +1000

    week1 lab

--- week1/exercise1.py
+++ week1/exercise1.py
@@ -1,9 +1,6 @@
-"""Your very first python program!
 
-TODO: write a python script that first prints "Hello world!"
-"""
-#print the hello world
+# """Your very first python program!
+# TODO: write a python script that first prints "Hello world!"
+# """
 
 print("Hello world!")
-
-
